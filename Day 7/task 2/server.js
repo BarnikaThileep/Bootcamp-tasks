c4d282464222da71225fa0319648ae0e4569c1db
Barnika L T <barnikathileepkumar@gmail.com>
2025-02-28 09:38:57 +0530

    Add files via upload

---
+++ Day 7/task 2/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const studentRoutes = require('./routes/students');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+// MongoDB connection
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/barnika')
+  .then(() => console.log('✅ Connected to MongoDB'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
+
+
+// Routes
+app.use('/students', studentRoutes);
+
+// Start the server
+app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
